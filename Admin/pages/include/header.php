1f12cecdeea34c4c03449d3fe4d0584351d85748
AbhijeetTugave <abhijeettugave@gmail.com>
2022-06-16 15:15:46 +0530

    s

--- Admin/pages/include/header.php
+++ Admin/pages/include/header.php
@@ -11,7 +11,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="dashboard-Admin.php" class="nav-link">Home</a>
+        <a href="index.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a class="btn btn-smb btn-light"  href="system-Reports.php" class="nav-link"><i class="far fa-clock" style="font-size:20px;"></i></a>
